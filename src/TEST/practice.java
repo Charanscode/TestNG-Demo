2a4789760e519ec385c5ea5777de618751540779
charanscode <charankumar6198@gmail.com>
2022-06-21 06:24:31 +0530

    demo commited

--- src/TEST/practice.java
+++ src/TEST/practice.java
@@ -8,10 +8,12 @@ public class practice {
 	public void p1()
 	{
 		System.out.println("good to start");
+		System.out.println("good to start1");
 	}
 	@Test
 	public void p2()
 	{
 		System.out.println("started");
+		System.out.println("started2");
 	}
 }
