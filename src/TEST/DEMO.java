a89ea72202e4d56fe355fd4e28740946d948ac36
charanscode <charankumar6198@gmail.com>
2022-06-21 05:42:07 +0530

    testNG first commit

---
+++ src/TEST/DEMO.java
@@ -0,0 +1,19 @@
+package TEST;
+
+import org.testng.annotations.Test;
+
+public class DEMO {
+
+	@Test
+	public void first() {
+
+		System.out.println("hello");
+	}
+
+	@Test
+	public void second()
+
+	{
+		System.out.println("Bye");
+	}
+}
---
+++ src/TEST/practice.java
@@ -0,0 +1,17 @@
+package TEST;
+
+import org.testng.annotations.Test;
+
+public class practice {
+
+	@Test
+	public void p1()
+	{
+		System.out.println("good to start");
+	}
+	@Test
+	public void p2()
+	{
+		System.out.println("started");
+	}
+}
